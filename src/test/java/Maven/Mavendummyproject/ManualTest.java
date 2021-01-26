f981a8b259fbdf64b54774eec726019cfe2a66ec
riyaz <pattanriyaz9@gmail.com>
2021-01-26 15:06:10 +0530

    R Commit Manual case updated

--- src/test/java/Maven/Mavendummyproject/ManualTest.java
+++ src/test/java/Maven/Mavendummyproject/ManualTest.java
@@ -11,7 +11,9 @@ public static void firsttestcase()
 System.out.println("Manual TEST FOR SMOKE TESTING");
 System.out.println("Update from Mater 1");
 System.out.println("Update from Mater 1");
+}{
+System.out.println("Modified from BRANCH X");
+System.out.println("Modified from BRANCH X");
 	}
-	
 
 }
