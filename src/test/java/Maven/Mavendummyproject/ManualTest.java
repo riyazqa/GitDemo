8594806dfda6816c6f678c923d20cf7c38230e34
riyaz <pattanriyaz9@gmail.com>
2021-01-26 12:47:08 +0530

    Riyaz firstcommit

---
+++ src/test/java/Maven/Mavendummyproject/ManualTest.java
@@ -0,0 +1,14 @@
+package Maven.Mavendummyproject;
+
+import org.testng.annotations.Test;
+
+public class ManualTest {
+
+	
+@Test
+public static void firsttestcase()
+{
+System.out.println("Manual TEST FOR SMOKE TESTING");
+	}
+
+}
---
+++ src/test/java/Maven/Mavendummyproject/SeleniumTest.java
@@ -0,0 +1,15 @@
+package Maven.Mavendummyproject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+@Test
+public static void siumtc()
+{
+	System.out.println("printing for REGRESSION TEST INDIVIDUALLY");
+}
+	}
+
+
